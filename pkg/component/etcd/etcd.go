699dd6bfe708e5899b6bb9bf231a4d0ca9e6faa0
Martin Weindel <martin.weindel@sap.com>
2023-05-24 11:05:36 +0200

    fix etcca datakey

--- pkg/component/etcd/etcd.go
+++ pkg/component/etcd/etcd.go
@@ -481,7 +481,7 @@ func (e *etcd) Deploy(ctx context.Context) error {
 			},
 		}
 		volumeData := map[string][]byte{
-			"tls/ca.crt":     etcdCASecret.Data["ca.crt"],
+			"tls/ca.crt":     etcdCASecret.Data[secretsutils.DataKeyCertificateBundle],
 			"tls/server.crt": serverSecret.Data["tls.crt"],
 			"tls/server.key": serverSecret.Data["tls.key"],
 		}
