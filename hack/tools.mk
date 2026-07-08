f1e850e8dadb03d6f3e84fb1241b1a07ddef87aa
Martin Weindel <martin.weindel@sap.com>
2026-07-08 14:47:20 +0200

    hack2

--- hack/tools.mk
+++ hack/tools.mk
@@ -16,8 +16,8 @@ GARDENER_HACK_DIR          := ./hack
 else
 # dependency on github.com/gardener/gardener is optional.
 # If other repos don't use it and the project doesn't depend on the package, silence the error to minimize confusion.
-GARDENER_HACK_DIR          := $(shell go list -m -f "{{.Dir}}" github.com/gardener/gardener/hack/tools/tool 2>/dev/null)/../..
-MODFILE_TOOL_MOD           := -modfile $(GARDENER_HACK_DIR)/tools/tool/go.mod
+GARDENER_HACK_DIR          := $(shell go list -m -f "{{.Dir}}" github.com/gardener/gardener 2>/dev/null)/hack
+MODFILE_TOOL_MOD           := -modfile $(shell go list -m -f "{{.Dir}}" github.com/gardener/gardener/hack/tools/tool 2>/dev/null)/go.mod
 SET_GOWORK                 := GOWORK=off
 endif
 
